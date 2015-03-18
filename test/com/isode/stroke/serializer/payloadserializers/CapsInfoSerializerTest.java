cdded9d208c58a48fd0c2802bd6ed533db46eeff
Tarun Gupta <tarun1995gupta@gmail.com>
2015-03-18 15:52:00 +0530

    Add Copyright Information for CapsInfo.
    License:
    This patch is BSD-licensed, see Documentation/Licenses/BSD-simplified.txt for details.

--- test/com/isode/stroke/serializer/payloadserializers/CapsInfoSerializerTest.java
+++ test/com/isode/stroke/serializer/payloadserializers/CapsInfoSerializerTest.java
@@ -1,11 +1,22 @@
+/*
+ * Copyright (c) 2010 Isode Limited.
+ * All rights reserved.
+ * See the COPYING file for more information.
+ */
+/*
+ * Copyright (c) 2015 Tarun Gupta.
+ * Licensed under the simplified BSD license.
+ * See Documentation/Licenses/BSD-simplified.txt for more information.
+ */
+
 package com.isode.stroke.serializer.payloadserializers;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import src.com.isode.stroke.elements.CapsInfo;
-import src.com.isode.stroke.serializer.payloadserializers.CapsInfoSerializer;
+import com.isode.stroke.elements.CapsInfo;
+import com.isode.stroke.serializer.payloadserializers.CapsInfoSerializer;
 
 public class CapsInfoSerializerTest {
 
