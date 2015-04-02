a20ca7ba40d837abe228462be0aba5d32d6831e3
Kevin Smith <git@kismith.co.uk>
2015-04-02 22:18:30 +0100

    Fix copyright header on DateTime
    Change-Id: I97fa434712eb32ad2c56558434a09fe5b1e3fb92

--- src/com/isode/stroke/base/DateTime.java
+++ src/com/isode/stroke/base/DateTime.java
@@ -1,12 +1,7 @@
 /*
-* Copyright (c) 2014 Kevin Smith and Remko Tronçon
-* All rights reserved.
-*/
-
-/*
-* Copyright (c) 2014, Isode Limited, London, England.
-* All rights reserved.
-*/
+ * Copyright (c) 2014-2015, Isode Limited, London, England.
+ * All rights reserved.
+ */
 
 package com.isode.stroke.base;
 
@@ -19,7 +14,7 @@ import java.util.TimeZone;
 public class DateTime {
     public static Date stringToDate(String date) {
         SimpleDateFormat parser = new SimpleDateFormat(format, Locale.US);
-        parser.setTimeZone(TimeZone.getTimeZone("UTC")); 
+        parser.setTimeZone(TimeZone.getTimeZone("UTC"));
         try {
             return parser.parse(date);
         } catch (ParseException e) {
@@ -29,9 +24,9 @@ public class DateTime {
 
     public static String dateToString(Date date) {
         SimpleDateFormat parser = new SimpleDateFormat(format, Locale.US);
-        parser.setTimeZone(TimeZone.getTimeZone("UTC")); 
+        parser.setTimeZone(TimeZone.getTimeZone("UTC"));
         return parser.format(date);
     }
-    
+
     static private String format = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 }
